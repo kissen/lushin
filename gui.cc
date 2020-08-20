fbfbeb2182a24e4ed85230c2b17cb172de39705f
Andreas Schärtl <andreas@schaertl.me>
2020-08-20 10:29:36 +0200

    gui: draw chess board

--- gui.cc
+++ gui.cc
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <iostream>
 #include <cstdio>
 #include <signal.h>
 
@@ -24,16 +25,16 @@ static SDL_Renderer *renderer;
 #define CELL_DIM 64
 
 static const SDL_Color SDL_BLACK = {
-	32,
-	194,
-	14,
+	0,
+	0,
+	0,
 	255
 };
 
 static const SDL_Color SDL_WHITE = {
-	0,
-	0,
-	0,
+	255,
+	255,
+	255,
 	255
 };
 
@@ -46,26 +47,41 @@ static void fail_with_sdl_error(const char *funcname)
 
 #define fail_with_sdl_error() fail_with_sdl_error(__func__)
 
-static void set_render_color(const SDL_Color *color)
+static void set_render_color(const SDL_Color &color)
 {
-	if (SDL_SetRenderDrawColor(renderer, color->r, color->g, color->b, color->a)) {
+	if (SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a)) {
 		fail_with_sdl_error();
 	}
 }
 
-static void begin_drawing(void)
+static void begin_drawing()
 {
-	set_render_color(&SDL_BLACK);
+	set_render_color(SDL_BLACK);
 
 	if (SDL_RenderClear(renderer)) {
 		fail_with_sdl_error();
 	}
 }
 
-static void end_drawing(void)
+static void end_drawing()
 {
 	SDL_RenderPresent(renderer);
 }
+
+static void draw_rectangle(uint8_t x, uint8_t y, const SDL_Color &color)
+{
+	const SDL_Rect rectangle = {
+		x * CELL_DIM, y * CELL_DIM,
+		CELL_DIM, CELL_DIM
+	};
+
+	set_render_color(color);
+
+	if (SDL_RenderFillRect(renderer, &rectangle)) {
+		fail_with_sdl_error();
+	}
+}
+
 //
 // implementation of gui.hh functions
 //
@@ -110,11 +126,33 @@ void gui::update()
 {
 }
 
+static void draw_background()
+{
+	static const SDL_Color background_colors[] = {
+		SDL_WHITE, SDL_BLACK
+	};
+
+	size_t cellid = 0;
+
+	for (uint8_t y = 0; y < 8; ++y) {
+		for (uint8_t x = 0; x < 8; ++x) {
+			const size_t idx = cellid++ % 2;
+			const SDL_Color &color = background_colors[idx];
+			draw_rectangle(x, y, color);
+		}
+
+		cellid += 1;
+	}
+}
+
 void gui::draw()
 {
 	assert(window);
 
 	begin_drawing();
+
+	draw_background();
+
 	end_drawing();
 }
 
